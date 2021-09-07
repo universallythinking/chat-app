9edacdbaf59c65ab23bb1f6ebd2c04bea6e0adea
Ben <Benjamin@universallthinking.com>
2021-09-07 11:48:43 -0400

    DEPLOY

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const path = require("path");
 const PORT = process.env.PORT || 8080;
 
 // REMOVE COMMENTS BELOW WHEN READY TO DEPLOY
- app.use(express.static(path.join(__dirname, "client/build")));
+ app.use(express.static(path.join(__dirname, "client/public")));
  app.get("/*", function (req, res) {
-   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+   res.sendFile(path.join(__dirname, "client/public", "index.html"));
  });
 
 let usersConnected = new Map();
